64687cc3cc17abe4a0a6d232d366a773e50c9e20
kleitonalbuquerque <kleiton2102@gmail.com>
2022-01-28 18:33:15 -0300

    Setup Express web server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
+var app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Rota principal",
+  });
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Ops! Alguma coisa deu errado!");
+  } else {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  }
+});
